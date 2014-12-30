d5f010dbd09bc8a757f733f043d63c00fb1b7d37
philipz <philipzheng@gmail.com>
2014-12-30 14:04:47 +0800

    In Memory version

--- src/InitRegression.java
+++ src/InitRegression.java
@@ -8,16 +8,77 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class InitRegression {
-	int inputt;
+	static int inputt;
 	int totalvol;
-	double SGXindex = 0;
+	static double SGXindex;
 
+	public static double[] init(String YYMMDD) {
+		String s1;
+		boolean notfound = true;
+		double[] result = {0,0};
+		while (notfound) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("C:\\Dropbox\\" + YYMMDD + "_APItick.txt"));
+			notfound = false;
+			InitRegression client = new InitRegression();
+			while ((s1 = in.readLine()) != null) {
+				client.doit(s1);
+			}
+			//client.closetxt(version);
+			result[0] = inputt;
+			result[1] = SGXindex;
+		} catch (FileNotFoundException e) {
+			SimpleDateFormat dateFormat=new SimpleDateFormat("yyyyMMdd");
+			GregorianCalendar cal = new GregorianCalendar();
+			Date nowdate;
+			try {
+				nowdate = dateFormat.parse(YYMMDD);
+				cal.setTime(nowdate);
+				cal.add(GregorianCalendar.DATE, -1);
+				YYMMDD = dateFormat.format(cal.getTime());
+			} catch (ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+		return result;
+	}
+	
+	public static double[] initNew(String YYMMDD) {
+		double[] result = {0,0};
+		double[] resultPre = {0,0};
+		double[] Gap = {0,0,0};
+		result = init(YYMMDD);
+		SimpleDateFormat dateFormat=new SimpleDateFormat("yyyyMMdd");
+		GregorianCalendar cal = new GregorianCalendar();
+		Date nowdate;
+		try {
+			nowdate = dateFormat.parse(YYMMDD);
+			cal.setTime(nowdate);
+			cal.add(GregorianCalendar.DATE, -1);
+			YYMMDD = dateFormat.format(cal.getTime());
+			resultPre = init(YYMMDD);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		double SGXGap = (((result[1] / resultPre[1]) - 1)) - ((result[0] / resultPre[0]) - 1);
+		Gap[0] = result[0];
+		Gap[1] = result[1];
+		Gap[2] = SGXGap;
+		return Gap;
+	}
+	
 	public static void init(String YYMMDD, int version) {
 		String s1;
 		boolean notfound = true;
 		while (notfound) {
 		try {
-			BufferedReader in = new BufferedReader(new FileReader("D:\\Dropbox\\" + YYMMDD + "_APItick.txt"));
+			BufferedReader in = new BufferedReader(new FileReader("C:\\Dropbox\\" + YYMMDD + "_APItick.txt"));
 			notfound = false;
 			InitRegression client = new InitRegression();
 			while ((s1 = in.readLine()) != null) {
@@ -68,4 +129,5 @@ public class InitRegression {
 		GoodWindowsExec.runwait("Copy FuT" + version + ".txt FuTemp" + version + ".txt");
 		GoodWindowsExec.runwait("Copy SGXT" + version + ".txt SGXTemp" + version + ".txt");
 	}
+	
 }
